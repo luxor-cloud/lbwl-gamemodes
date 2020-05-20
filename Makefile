e8c86df68a54dbdbac8cb7379fd61980e3d510a2
Yannic Rieger <riegeryannic@gmail.com>
2020-05-20 23:32:37 +0200

    Get jar from nexus

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 DOCKER=$(shell which docker)
 
-# TODO: build docker image
 mode_flash:
-    ./metadata.py --update flash
+	./metadata.py --update flash
 	cd flash && ./collect.py
--- collect.py
+++ collect.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
 import os
 import pysftp
 import zipfile
+import urllib.request
 
 def downloadAndUnzip(remote_path, local_path):
-  print 'DOWNLOAD ' + remote_path.format('spawn', data['spawn']['version'])
+  print('DOWNLOAD > {}'.format(remote_path.format('spawn', data['spawn']['version'])))
   sftp.get(remote_path, local_path)
   with zipfile.ZipFile(local_path, 'r') as zip_ref:
     zip_ref.extractall(local_path[:-4])
@@ -21,13 +22,21 @@ os.mkdir('.work/maps')
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None 
 
-host = os.environ['MAP_STORAGE_HOST']
-user = os.environ['MAP_STORAGE_USER']
-passwd = os.environ['MAP_STORAGE_PASSWORD']
+storage_host = os.environ['MAP_STORAGE_HOST']
+storage_user = os.environ['MAP_STORAGE_USER']
+storage_passwd = os.environ['MAP_STORAGE_PASSWORD']
 
-# TODO: download game jar
+nexus_host = os.environ['MVN_REPO_HOST']
+nexus_user = os.environ['MVN_REPO_USER']
+nexus_passwd = os.environ['MVN_REPO_PASSWORD']
 
-with pysftp.Connection(host=host, username=user, password=passwd, cnopts=cnopts) as sftp:
+mode_name = data['jar']['name']
+mode_version = data['jar']['version']
+
+url = 'http://{user}:{passwd}@{host}/repository/lbwl-mvn-releases/cloud/luxor/lbwl/{game}/{version}/{game}-{version}.jar'
+urllib.request.urlretrieve(url.format(user=nexus_user, passwd=nexus_passwd, game=mode_name, version=mode_version, '.work/{}-{}'.format(mode_name, mode_version))
+
+with pysftp.Connection(host=storage_host, username=storage_user, password=storage_passwd, cnopts=cnopts) as sftp:
   remote_path = '/lbwl/maps/flash/{}/{}.zip'
   downloadAndUnzip(remote_path.format('spawn', data['spawn']['version']), '.work/spawn.zip')
   for v in data['maps']:
