bc3d428493bc075d04d12b8d35dea6d934a98eb0
Yannic Rieger <riegeryannic@gmail.com>
2020-05-21 13:12:45 +0200

    Add mode version to mode.json

--- metadata.py
+++ metadata.py
@@ -7,7 +7,13 @@ from checksumdir import dirhash
 parser = argparse.ArgumentParser()
 parser.add_argument('--update')
 parser.add_argument('--mode')
+parser.add_argument('--mode-version')
 args = parser.parse_args()
+
+def read_mode_file(mode):
+  with open('{}/mode.json'.format(mode), 'r') as json_file:
+    data = json.load(json_file)
+    return data
 
 if args.update:
   sha1hash = dirhash(args.update, 'sha1', excluded_files=['collect.py', 'Dockerfile'], ignore_hidden=True)
@@ -22,3 +28,6 @@ if args.mode:
   with open('.metadata', 'r') as json_file:
     data = json.load(json_file)
     print(data[args.mode])
+
+if args.mode_version:
+  print(read_mode_file(args.mode_version)['version'])
