86905563f704008d9b82a153318beaae91cf1054
Yannic Rieger <riegeryannic@gmail.com>
2020-06-10 21:31:47 +0200

    Finish build script

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-docker login docker.luxor.cloud -u test -p test
+docker login $DOCKER_REPO_URL -u $DOCKER_REPO_USER -p $DOCKER_REPO_PASSWD
 
 for d in */ ; do
   mode=$(basename $d)
   ver=$(./metadata.py --mode-version $mode)
+  
+  echo 'INFO lbwl-$mode v$ver'
+  cd flash && ./collect.py && cd ..
+
   exists=$(docker manifest inspect lbwl-$mode:$ver > /dev/null ; echo $?)
-  echo $exists
+  if [ $exists == 0 ]; then
+    tag=$DOCKER_REPO_URL/lbwl-$mode:$ver
+    docker build -t $DOCKER_REPO_URL/$tag -f $dDockerfile $d
+    docker push $tag
+  else 
+    echo 'INFO lbwl-$mode v$ver already exists, not updating'
+  fi
 done
