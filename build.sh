2c63935d156317cb400114dd2bb14133799f2248
Yannic Rieger <riegeryannic@gmail.com>
2020-06-11 00:38:57 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 docker login $DOCKER_REPO_URL -u $DOCKER_REPO_USER -p $DOCKER_REPO_PASSWORD
 
 for d in */ ; do
@@ -9,7 +11,7 @@ for d in */ ; do
   echo "INFO lbwl-$mode v$ver"
   cd flash && ./collect.py && cd ..
 
-  exists=$(docker manifest inspect lbwl-$mode:$ver > /dev/null ; echo $?)
+  exists=$(docker manifest inspect $DOCKER_REPO_URL/lbwl-$mode:$ver > /dev/null ; echo $?)
   if [ $exists == 0 ]; then
     tag=$DOCKER_REPO_URL/lbwl-$mode:$ver
     docker build -t $DOCKER_REPO_URL/$tag -f $dDockerfile $d
