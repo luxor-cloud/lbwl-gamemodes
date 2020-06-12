2c620857873b1abe92a9546bceee280cd5c39357
Yannic Rieger <riegeryannic@gmail.com>
2020-06-12 19:17:21 +0200

    Check if image exists before file download

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ for d in */ ; do
   mode=$(basename $d)
   ver=$(./metadata.py --mode-version $mode)
   echo "INFO lbwl-$mode v$ver"
-  cd $d
-  ./collect.py
-  cd ..
   tag=$DOCKER_REPO_URL/lbwl-$mode:$ver
   sudo chmod o+x /etc/docker
   exists=$(docker manifest inspect $tag > /dev/null ; echo $?)
   if [ $exists == 1 ]; then
+    cd $d
+    ./collect.py
+    cd ..
     docker build -t $tag -f $mode/Dockerfile $d
     docker push $tag
   else 
